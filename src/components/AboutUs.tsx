51d6158c2b8cb9a45a37fc40153e8ef62c69a053
出口レイ <richard@kanshen.click>
2025-01-10 17:05:08 +0100

    Fixed up some code
    God this code quality is awful

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,23 +1,17 @@
-import { ReactSVG } from "react-svg"
 import kolotoc from "@/images/AboutUsImage.jpg"
 
-
-
-export default function AboutUs(){
+export default function AboutUs() {
     return (
         <article className="flex flex-col justify-center items-center">
             <div className="flex flex-row-reverse mobile:flex-col">
-            <img  src={kolotoc} className="mx-auto" />
+                <img src={kolotoc} className="mx-auto" />
                 <div className="flex flex-col notMobile:justify-end">
-                <p className="text-6xl text-accentPlus font-bold  pb-[15px] mobile:text-center" >O nás</p>
-                <p className="text-xl w-[500px] pb-[150px] mobile:text-center">
-                Naša rodina sa venuje prevádzkovaniu atrakcií už dlhé roky.
-                Lunapark Radovana Kudriho je 5.generácia a  poskutuje zábavu 
-                pre rôzne vekové kategórie hlavne na strednom a západnom slovensku.
-</p>
-            </div></div>
-        
+                    <p className="text-6xl text-accentPlus font-bold  pb-[15px] mobile:text-center" >O nás</p>
+                    <p className="text-xl w-[500px] pb-[150px] mobile:text-center">
+                        Naša rodina sa venuje prevádzkovaniu atrakcií už dlhé roky.
+                        Lunapark Radovana Kudriho je 5. generácia a poskytuje zábavu pre rôzne vekové kategórie hlavne na strednom a západnom slovensku.
+                    </p>
+                </div></div>
         </article>
     );
-
 }
--- src/components/BackgroundImageArticle.tsx
+++ src/components/BackgroundImageArticle.tsx
@@ -1,14 +1,8 @@
-import { ReactSVG } from "react-svg"
 import kolotocekudri from "@/images/BGImage.jpg"
-
-
-export default function BackgroundImageArticle(){
-return (
-    
-    <article className="background h-screen bg-cover bg-center w-full   md:w-full justify-center py-10 md:px-[450px]" style={{backgroundImage:`url("${kolotocekudri}")`}}>
-        <div className="text-5xl md:text-7xl text-accentPlus">Kolotoče Kudri<div className="text-accent text-4xl md:text-6xl">Roztočme zážitky s úsmevom</div></div> 
-    </article>
-    
-    
-)
+export default function BackgroundImageArticle() {
+    return (
+        <article className="background h-screen bg-cover bg-center w-full md:w-full justify-center py-10 md:px-[450px]" style={{ backgroundImage: `url("${kolotocekudri}")` }} >
+            <div className="text-5xl md:text-7xl text-accentPlus">Kolotoče Kudri<div className="text-accent text-4xl md:text-6xl">Roztočme zážitky s úsmevom</div></div>
+        </article>
+    );
 }
