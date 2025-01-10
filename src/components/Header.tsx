5ec56fc4a8fcb0000a6e83304af685a981d467ff
出口レイ <richard@kanshen.click>
2025-01-10 16:07:14 +0100

    made some minor changes to distinguish current link
    Revert "made some minor changes to distinguish current link"
    
    This reverts commit 43e314acf8842b3e6997dfd698e150b129c12f62.
    
    Reapply "made some minor changes to distinguish current link"
    
    This reverts commit b0b87f354e83411cdb67d8524b1bdbbf9df8d231.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { ReactSVG } from 'react-svg';
 import ferrisWheel from "@/images/ferrisWheel.svg"
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useRef, useState, useEffect } from 'react';
 
 export default function Header() {
@@ -56,14 +56,14 @@ export default function Header() {
                 <div className="text-2xl font-bold">Kolotoče Kudri</div>
             </div>
             <div className='flex flex-row gap-3 items-center mobileWrap:hidden'>
-                {Object.entries(links).map(([key, value]) => (<Link to={value} key={key} className="text-xl">{key}</Link>))}
+                {Object.entries(links).map(([key, value]) => (<Link to={value} key={key} className={`text-xl ${useLocation().pathname === value ? "font-bold italic" : ""}`}>{key}</Link>))}
             </div>
             <div className='group/checkbox hidden mobileWrap:flex flex-row items-center before:bg-[rgba(0,0,0,0)] has-[:checked]:before:bg-[rgba(0,0,0,0.75)] before:backgroup-blue-none has-[:checked]:before:backdrop-blur-sm before:pointer-events-none has-[:checked]:before:pointer-events-auto before:fixed before:top-0 before:left-0 before:w-[100%] before:h-[100%] before:transition-[background-color,backdrop-filter] before:duration-500 before:ease-in-out' ref={mobileMenuRef}>
                 <input type="checkbox" id="mobileMenuOpen" className='hidden peer' checked={isMobileMenuOpen} onChange={(e) => setIsMobileMenuOpen(e.target.checked)} />
                 <label className='material-symbols cursor-pointer' htmlFor="mobileMenuOpen">menu</label>
                 <div className='flex flex-col fixed right-[-75%] transition-right duration-500 ease-in-out peer-checked:right-0 top-0 w-[75%] h-[100%] bg-accentMinus text-white selection:bg-white selection:text-accentMinus select-none font-bold p-16 pt-24 gap-y-5 *:cursor-pointer *:text-[32px] z-[1]'>
                     <label className='material-symbols fixed -right-[75%] transition-right duration-500 ease-in-out group-has-[:checked]/checkbox:right-5 top-4 h-6 w-6' htmlFor="mobileMenuOpen">close</label>
-                    {Object.entries(links).map(([key, value]) => (<Link to={value} key={key} className="leading-6 w-fit" onClick={() => setIsMobileMenuOpen(false)}>{key}</Link>))}
+                    {Object.entries(links).map(([key, value]) => (<Link to={value} key={key} className={`leading-6 w-fit ${useLocation().pathname === value ? "font-bold italic tracking-widest" : ""}`} onClick={() => setIsMobileMenuOpen(false)}>{key}</Link>))}
                 </div>
             </div>
         </div>
