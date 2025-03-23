d714d7e9c8035317bf187b0443215dd9436d7569
出口レイ <richard@kanshen.click>
2025-03-23 20:00:06 +0100

    wtf

--- scripts/webpify.js
+++ scripts/webpify.js
@@ -91,6 +91,7 @@ async function replaceInFiles() {
     for (const filePath of codeFiles) {
         try {
             let content = await fs.readFile(filePath, 'utf8');
+            console.log(content)
             let hasChanges = false;
 
             // Handle SVG imports and references
