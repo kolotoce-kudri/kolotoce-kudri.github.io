5997cd4fd172bb345c4510d2299186c8cde84ae7
出口レイ <richard@kanshen.click>
2025-03-23 20:06:36 +0100

    update to bun because node hates me

--- scripts/webpify.js
+++ scripts/webpify.js
@@ -91,7 +91,6 @@ async function replaceInFiles() {
     for (const filePath of codeFiles) {
         try {
             let content = await fs.readFile(filePath, 'utf8');
-            console.log(filePath, Boolean(content))
             let hasChanges = false;
 
             // Handle SVG imports and references
