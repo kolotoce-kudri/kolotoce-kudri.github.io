72370ffbc455874ff4acaccdcdc22b4bd7ceae6e
出口レイ <richard@kanshen.click>
2025-03-24 07:38:17 +0100

    oopsie

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
-//import { ReactSVG } from "react-svg";
-//import ferrisWheel from "@/images/ferrisWheel.svg";
+import { ReactSVG } from 'react-svg';
+import ferrisWheel from "@/images/ferrisWheel.svg"
+import { useRef, useState, useEffect } from 'react';
 
 export default function Header() {
-    return (<></>);
+    return (
+        <div className="sticky top-0 flex items-center justify-center bg-accent text-accentPlus selection:bg-accentPlus selection:text-accent select-none px-4 py-3 z-50">
+            <div className='flex flex-row gap-2 items-center'>
+                <ReactSVG src={ferrisWheel} beforeInjection={(svg) => svg.classList.add("fill-accentPlus", "w-[40px]", "h-[40px]")} />
+                <div className="text-2xl font-bold">Kolotoče Kudri</div>
+            </div>
+        </div>
+    ) as React.JSX.Element
 }
